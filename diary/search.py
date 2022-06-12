48c21cbb39792f5a9a5715f1c518761736d98f16
emmacasey <36385079+emmacasey@users.noreply.github.com>
2022-06-12 20:41:42 +0100

    docstrings

--- diary/search.py
+++ diary/search.py
@@ -5,12 +5,16 @@ from .core import Diary, Record
 
 
 def strict_search(diary: Diary, search_term: str) -> list[Record]:
+    """For a given diary return a list of records where the search term is a substring
+    of the record's text."""
     return [record for record in diary.records if search_term in record.text]
 
 
 def date_filter(
     records: list[Record], *, before: Optional[str] = None, after: Optional[str] = None
 ) -> list[Record]:
+    """Filter a list of records to only those before and after given timestamps,
+    both terms are optional, and both should be iso strings."""
     return [
         record
         for record in records
@@ -27,6 +31,8 @@ def metric_filter(
     lt: Optional[float] = None,
     eq: Optional[float] = None,
 ) -> list[Record]:
+    """ "Filter a list of records to only those with a given metric, and of those
+    only those greater than, less than, and equal to the given (optional) values."""
     return [
         record
         for record in records
