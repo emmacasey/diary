6fbf87944694506f33ab639e1032cc9d6e735299
emmacasey <36385079+emmacasey@users.noreply.github.com>
2022-06-25 16:43:50 +0100

    nlp experiements

--- diary/nlp.py
+++ diary/nlp.py
@@ -1,18 +1,41 @@
 import nltk
+import string
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 nltk.download("punkt")
 nltk.download("stopwords")
+nltk.download("vader_lexicon")
 
 stemmer = nltk.PorterStemmer()
 stopwords = nltk.corpus.stopwords.words("english")
+sentiment_analyser = SentimentIntensityAnalyzer()
+word_characters = string.ascii_letters + string.whitespace + string.digits
+
+
+def strip_punc(sent: str) -> str:
+    return "".join(c for c in sent if c in word_characters)
 
 
 def tokenize(sent: str) -> list[str]:
     """Given some prose produe lematised tokens with stopwords stripped."""
-    tokens = nltk.tokenize.word_tokenize(
-        sent.replace("-", " ").replace("—", " ").lower()
-    )
-    content_words = [
-        word for word in tokens if word not in stopwords and word.isalpha()
-    ]
-    return [stemmer.stem(word, to_lowercase=True) for word in content_words]
+    tokens = word_tokenize(strip_punc(sent).lower())
+    return [stemmer.stem(word) for word in tokens if word not in stopwords]
+
+
+def stats(para: str) -> dict[str, float]:
+    sents = sent_tokenize(para)
+    tokens = word_tokenize(para)
+    content_lemmas = tokenize(para)
+    return sentiment(para) | {
+        "sent_count": len(sents),
+        "token_count": len(tokens),
+        "content_words": len(content_lemmas),
+        "letters_per_word": sum(len(word) for word in tokens) / len(tokens),
+        "words_per_sent": len(tokens) / len(sents),
+        "content_per_sent": len(content_lemmas) / len(sents),
+    }
+
+
+def sentiment(sent: str) -> dict[str, float]:
+    return sentiment_analyser.polarity_scores(sent)
--- tests/test_nlp.py
+++ tests/test_nlp.py
@@ -1,16 +1,34 @@
 import unittest
 
-from diary.nlp import tokenize
+from diary.nlp import tokenize, strip_punc, sentiment, stats
 
 
 class TestPipelines(unittest.TestCase):
+    def setUp(self):
+        self.para = """3 May. Bistritz.—
+        Left Munich at 8:35 P. M., on 1st May, arriving at Vienna early next morning; should have arrived at 6:46, but train was an hour late."""
+        self.pos = "Buda-Pesth seems a wonderful place, from the glimpse which I got of it from the train and the little I could walk through the streets."
+        self.neg = "I feared to go very far from the station, as we had arrived late and would start as near the correct time as possible."
+
+    def test_punctuation_stripper(self):
+        self.assertEquals(
+            strip_punc(self.para),
+            """3 May Bistritz
+        Left Munich at 835 P M on 1st May arriving at Vienna early next morning should have arrived at 646 but train was an hour late""",
+        )
+
     def test_tokenizer(self):
         self.assertEquals(
+            tokenize(self.para),
             [
+                "3",
                 "may",
                 "bistritz",
                 "left",
                 "munich",
+                "835",
+                "p",
+                "1st",
                 "may",
                 "arriv",
                 "vienna",
@@ -18,11 +36,43 @@ class TestPipelines(unittest.TestCase):
                 "next",
                 "morn",
                 "arriv",
+                "646",
                 "train",
                 "hour",
                 "late",
             ],
-            tokenize(
-                "3 May. Bistritz.—Left Munich at 8:35 P. M., on 1st May, arriving at Vienna early next morning; should have arrived at 6:46, but train was an hour late."
-            ),
+        )
+
+    def test_sentiment(self):
+        neutral = sentiment(self.para)
+        self.assertEqual(neutral["compound"], 0)
+        self.assertEqual(neutral["pos"], 0)
+        self.assertEqual(neutral["neg"], 0)
+        self.assertGreater(neutral["neu"], 0)
+
+        pos = sentiment(self.pos)
+        self.assertGreater(pos["compound"], 0)
+        self.assertGreater(pos["pos"], 0)
+        self.assertEqual(pos["neg"], 0)
+
+        neg = sentiment(self.neg)
+        self.assertLess(neg["compound"], 0)
+        self.assertGreater(neg["neg"], 0)
+        self.assertEqual(neg["pos"], 0)
+
+    def test_stats(self):
+        self.assertEqual(
+            stats(self.para),
+            {
+                "compound": 0.0,
+                "content_per_sent": 9.5,
+                "content_words": 19,
+                "letters_per_word": 124 / 35,
+                "neg": 0.0,
+                "neu": 1.0,
+                "pos": 0.0,
+                "sent_count": 2,
+                "token_count": 35,
+                "words_per_sent": 17.5,
+            },
         )
